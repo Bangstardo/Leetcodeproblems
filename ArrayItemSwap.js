03f1cb179374ec75d4d7ee8297470f7b4e3f58fb
Bangstardo <luisja0402@hotmail.com>
2023-11-25 15:03:11 +0100

    array nad bigo commit

---
+++ ArrayItemSwap.js
@@ -0,0 +1,21 @@
+// Got these ones from ThePrimeagen thanks to him 
+// bubble sort algorithm 
+
+export default function bubble_sort(arr: number[]): void {
+
+    // for loop that iterates through elements of a list
+
+    for (let i = 0; i < arr.length; i++) {
+
+        // 
+
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        } 
+    }
+}
