2f10cb5c2eb6f5f5439e179a8a1d8e99398b2737
Bangstardo <luisja0402@hotmail.com>
2023-11-26 02:47:00 +0100

    comment commit

--- ArrayItemSwap.js
+++ ArrayItemSwap.js
@@ -1,5 +1,6 @@
+// Breaking the convention of the Readme file this is not a problem that was submited to leetcode
+// This is rather a must know sorting bubble algorithm
 // Got these ones from ThePrimeagen thanks to him 
-// bubble sort algorithm 
 
 export default function bubble_sort(arr: number[]): void {
 
@@ -7,7 +8,7 @@ export default function bubble_sort(arr: number[]): void {
 
     for (let i = 0; i < arr.length; i++) {
 
-        // 
+        //  
 
         for (let j = 0; j < arr.length - 1 - i; j++) {
 
